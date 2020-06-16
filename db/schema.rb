af48d8f23f37525b3637643d977663a729482599
Toni  Diep <adiep123@gmail.com>
2020-06-16 03:07:28 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "artists", force: :cascade do |t|
     t.text "name"
     t.text "genre"
     t.integer "age"
     t.text "hometown"
+    # t.string "favorite_food"
   end
 
 end
